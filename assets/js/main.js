ea3fa6566d36a6d18e769343d318efb723b88200
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-08 17:32:06 +0100

    rgionamento snack 3 con math min

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,11 +170,44 @@
 
 //SNACK 3 
 // Snack 3
-// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+let racingBike = [
+  {
+    nome:'Atala',
+    peso: 10
+  },
+  {
+    nome:'Bianchi',
+    peso: 6
+  },
+  {
+    nome:'Bottecchia',
+    peso: 9
+  },
+  {
+    nome:'Olmo',
+    peso: 7
+  },
+];
+
+let pesoMinore = 0;
+
+
+  
+for (let i=0; i <racingBike.length; i++ ){
+let pesiBike = racingBike[i].peso;
+pesoMinore = Math.min(racingBike[i].peso);
+
+//console.log(pesiBike);
+};
 
 
 
-
+//si può fare con un oggetto singolo 
+const { nome,peso } = racingBike[0];
+//console.log(peso);
 
 
 
